d4e50b1a1b999df3616c089bcf8f57f440143773
Hunter Treadaway <51298555+huntertread@users.noreply.github.com>
2020-05-06 12:27:53 -0700

    Updates

---
+++ precache-manifest.aacfc3dfbe1bc0907c870ec7a82ab34e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ef7a8077f476de8b636f488beb171b",
+    "url": "/wzrd-client/index.html"
+  },
+  {
+    "revision": "6ef92443222f2f1f8a79",
+    "url": "/wzrd-client/static/css/main.22c16f0c.chunk.css"
+  },
+  {
+    "revision": "0002b98d09c2144e52e9",
+    "url": "/wzrd-client/static/js/2.a916be23.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/wzrd-client/static/js/2.a916be23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef92443222f2f1f8a79",
+    "url": "/wzrd-client/static/js/main.8df630b8.chunk.js"
+  },
+  {
+    "revision": "22cbc77a8c0f27c8308f",
+    "url": "/wzrd-client/static/js/runtime-main.7c6776b3.js"
+  }
+]);

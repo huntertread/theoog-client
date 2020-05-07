792044a1afaffaab9a7f6d5ebfa6ceb8a4b90eee
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2020-05-07 11:01:41 -0700

    Updates

---
+++ precache-manifest.6934cd0e89bcb29f35a4ec1a1c1e2e7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce85010549d73a2e2c416e2d1ac8c36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eb27928f0204d611259",
+    "url": "/static/css/main.ad03a9a1.chunk.css"
+  },
+  {
+    "revision": "0002b98d09c2144e52e9",
+    "url": "/static/js/2.a916be23.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.a916be23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb27928f0204d611259",
+    "url": "/static/js/main.07ac1147.chunk.js"
+  },
+  {
+    "revision": "d8b07a65e34a58f0450e",
+    "url": "/static/js/runtime-main.2a3eacec.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wzrd-client/precache-manifest.89bdc2f941862a9bbe179dca8866f425.js"
+  "/precache-manifest.6934cd0e89bcb29f35a4ec1a1c1e2e7e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wzrd-client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
